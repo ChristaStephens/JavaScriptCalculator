4d284e9ac81793ff2068cae25a99f519f644e673
Christa Stephens <ChristaStephens@outlook.com>
2019-02-27 18:07:07 -0500

    initial commit

---
+++ Calculator.js
@@ -0,0 +1,8 @@
+function calc(){
+    //grab values from the form
+    //calc them
+    //output them to the website
+    //DOM - element from current website
+    //creating a variable to use for the DOM
+    var a = document.querySelector("#value1");
+}
