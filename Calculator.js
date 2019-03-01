70c3d0f2fcb84993a816e1308bd143c69d126702
Christa Stephens <ChristaStephens@outlook.com>
2019-02-28 20:51:22 -0500

    added additional information to function

--- Calculator.js
+++ Calculator.js
@@ -4,5 +4,22 @@ function calc(){
     //output them to the website
     //DOM - element from current website
     //creating a variable to use for the DOM
-    var a = document.querySelector("#value1");
+    //"value" is  a string and we neet to convert it
+    //we use .notion to pull info from our html over
+    var a = parseInt(document.querySelector("#value1").value);
+    var b = parseInt(document.querySelector("#value2").value);
+        //operator is already a string, don't convert
+    var op = (document.querySelector("#operator").value);
+    var calculate;
+
+    if (op =="add"){
+        calculate = a+b;
+
+    } else if(op =="min"){
+        calculate = a+b;
+    }else if(op =="div"){
+        calculate = a+b;
+    }else if(op =="mul"){
+        calculate = a+b;
+    }
 }
